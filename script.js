177de30f3b2e0d7f869aed58566545b041e5a5ea
JTor <t.jor13@gmail.com>
2022-10-05 10:58:37 -0400

    Begin javascript conceptualizing

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+*   Create 16x16 grid of square divs using javascript
+*   Put them inside div container that is already in HTML
+*   use flexbox to make them appear as grid
+*
+*   Create hover effect so the grid divs change color as mouse passes over them
+*   should leave a pixelated trail
+*   Use event listeners for entering and leaving divs
+*
+*   Add button that asks user for new grid size
+*   Existing grid should be replaced with new grid in same space(e.g. 960px wide)
+*   E.g.  Entering 64 returns a 64x64 grid without changing the 
+*   total amount of pixels used
+*   Set user input limit to a max of 100
+*/
